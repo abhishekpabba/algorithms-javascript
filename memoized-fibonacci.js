7ece6d3ada84ed926b82d1bc4a6d7f1237744443
Abhishek Pabba <pabba@Abhisheks-MBP.attlocal.net>
2019-10-09 23:29:26 -0500

    graphs and hash tables

--- memoized-fibonacci.js
+++ memoized-fibonacci.js
@@ -15,6 +15,15 @@ function fibMemo(index, cache) {
 
 fibMemo(7);
 
+// Simplified
+function fib(n, memo=[]) {
+  if (memo[n]) return memo[n];
+  if (n <= 2) return 1;
+  memo[n] = fib(n-1, memo) + fib(n-2, memo);
+
+  return memo[n];
+}
+
 // Alternative Solution
 // function memoize(fn) {
 //   const cache = {};
